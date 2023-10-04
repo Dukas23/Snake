c3e28933111d38fd8e87a19813b0415867dd69e1
Juan Manuel Carvajal <jmcr-@hotmail.com.ar>
2023-10-04 14:31:09 -0500

    modiciones y adicion de dijkstra

--- src/snakeP2.py
+++ src/snakeP2.py
@@ -1,8 +1,9 @@
 import pygame as pg
 from pygame.math import Vector2 as V2
 from random import randint
+import numpy as np
 import heapq
-#!codigo completamente malo
+#
 pg.init()
 
 WIDTH = 600
@@ -20,75 +21,48 @@ SNAKE_SPEED = 10
 snake = [V2(7, 7)]
 direction = V2(1, 0)
 
-def calcular_camino_dijkstra(grafo, inicio, objetivo):
-    distancias = {nodo: float('inf') for nodo in grafo}
-    distancias[inicio] = 0
-    cola = [(0, inicio)]
-
-    while cola:
-        distancia_actual, nodo_actual = heapq.heappop(cola)
-
-        if nodo_actual == objetivo:
-            break
-
-        if distancia_actual > distancias[nodo_actual]:
-            continue
-
-        for vecino in grafo[nodo_actual]:
-            nueva_distancia = distancia_actual + 1  
-            if nueva_distancia < distancias[vecino]:
-                distancias[vecino] = nueva_distancia
-                heapq.heappush(cola, (nueva_distancia, vecino))
-
-    # Calcular camino completo
-    camino = []
-    nodo_actual = objetivo
-
-    while nodo_actual != inicio:
-        camino.append(nodo_actual)
-        for vecino in grafo[nodo_actual]:
-            if distancias[vecino] < distancias[nodo_actual]:
-                nodo_actual = vecino
-                break
-
-    camino.reverse()
-    return camino
 
 def draw_snake(SNAKE):
     for segment in SNAKE:
         x_2 = int(segment.x * GRID_SIZE)
         y_2 = int(segment.y * GRID_SIZE)
         pg.draw.rect(SCREEN, GREEN, (x_2, y_2, GRID_SIZE, GRID_SIZE))
-        
+
+
 def generate_food():
-    x_1 = randint(0, WIDTH//GRID_SIZE - 1)
-    y_1 = randint(0, HEIGHT//GRID_SIZE - 1)
-    food_position = V2(x_1, y_1)
-    return food_position
+    while True:
+        x_1 = randint(0, WIDTH//GRID_SIZE - 1)
+        y_1 = randint(0, HEIGHT//GRID_SIZE - 1)
+        food_position = V2(x_1, y_1)
+        if food_position not in snake:
+            return food_position
+
 
 food = generate_food()
 
+# Matriz para el algoritmo de Dijkstra
+distances = np.inf * np.ones((WIDTH//GRID_SIZE, HEIGHT//GRID_SIZE))
+distances[int(food.x)][int(food.y)] = 0
+
+
+def dijkstra():
+    queue = [(0, tuple(map(int, food)))]
+    while queue:
+        dist, pos = heapq.heappop(queue)
+        pos = V2(*pos)
+        for neighbor in [pos + V2(1, 0), pos + V2(-1, 0), pos + V2(0, 1), pos + V2(0, -1)]:
+            if 0 <= neighbor.x < WIDTH//GRID_SIZE and 0 <= neighbor.y < HEIGHT//GRID_SIZE:
+                new_dist = dist + 1
+                if new_dist < distances[int(neighbor.x)][int(neighbor.y)]:
+                    distances[int(neighbor.x)][int(neighbor.y)] = new_dist
+                    heapq.heappush(
+                        queue, (new_dist, tuple(map(int, neighbor))))
+
+
+dijkstra()
+
 GAME_OVER = False
 clock = pg.time.Clock()
-
-# Crear grafo
-grafo = {}
-for y in range(HEIGHT // GRID_SIZE):
-    for x in range(WIDTH // GRID_SIZE):
-        nodo = (x, y)
-        vecinos = []
-
-        if x > 0:
-            vecinos.append((x - 1, y))
-        if x < (WIDTH // GRID_SIZE - 1):
-            vecinos.append((x + 1, y))
-
-        if y > 0:
-            vecinos.append((x, y - 1))
-        if y < (HEIGHT // GRID_SIZE - 1):
-            vecinos.append((x, y + 1))
-
-        grafo[nodo] = vecinos
 
 while not GAME_OVER:
 
@@ -96,31 +70,52 @@ while not GAME_OVER:
         if event.type == pg.QUIT:
             GAME_OVER = True
 
-    inicio = (int(snake[0].x // GRID_SIZE), int(snake[0].y // GRID_SIZE))
-    objetivo = (int(food.x // GRID_SIZE), int(food.y // GRID_SIZE)) 
-    camino = calcular_camino_dijkstra(grafo, inicio, objetivo)
+    # Movimiento automático basado en Dijkstra
+    head = snake[0]
+    neighbors = [head + V2(1, 0), head + V2(-1, 0),
+                 head + V2(0, 1), head + V2(0, -1)]
+    min_distance = np.inf
+    next_direction = direction
+    for neighbor in neighbors:
+        if (
+            0 <= neighbor.x < WIDTH//GRID_SIZE
+            and 0 <= neighbor.y < HEIGHT//GRID_SIZE
+            and distances[int(neighbor.x)][int(neighbor.y)] < min_distance
+        ):
+            min_distance = distances[int(neighbor.x)][int(neighbor.y)]
+            next_direction = neighbor - head
 
-    if camino:
-        siguiente = V2(camino[0][0] - inicio[0], camino[0][1] - inicio[1])
-        snake.insert(0, snake[0] + siguiente)
-    else:
-        # Si no hay camino, la serpiente sigue en la misma dirección
-        snake.insert(0, snake[0] + direction)
+    direction = next_direction
 
-   # Comer comida
+    # Mover serpiente
+    snake.insert(0, snake[0] + direction)
+
+    # Comprobar colisión con bordes
+    if snake[0].x < 0 or snake[0].x >= WIDTH//GRID_SIZE:
+        GAME_OVER = True
+    if snake[0].y < 0 or snake[0].y >= HEIGHT//GRID_SIZE:
+        GAME_OVER = True
+
+    # Comprobar colisión consigo misma
+    if len(snake) > 1 and snake[0] in snake[1:]:
+        GAME_OVER = True
+
+    # Comer comida
     if snake[0] == food:
         food = generate_food()
+        dijkstra()  # Recalcular la ruta después de comer
     else:
         snake.pop()
 
-    # Dibujar y actualizar pantalla
     SCREEN.fill(BLACK)
+
     draw_snake(snake)
 
     x = int(food.x * GRID_SIZE)
     y = int(food.y * GRID_SIZE)
     pg.draw.rect(SCREEN, RED, (x, y, GRID_SIZE, GRID_SIZE))
+
     pg.display.update()
-    clock.tick(SNAKE_SPEED) 
+    clock.tick(SNAKE_SPEED)
 
 pg.quit()
